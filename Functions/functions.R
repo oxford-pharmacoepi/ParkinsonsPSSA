36f246ed3ec333a695d9bac88d68862a5711a99d
xihang-chen <xihang.chen@ndorms.ox.ac.uk>
2023-09-22 11:06:20 +0100

    new func

--- Functions/functions.R
+++ Functions/functions.R
@@ -292,6 +292,20 @@ getWaitingTimeDistribution <- function(cdm,
 return(p)
 }
 
+# pssa based on a subset of a condition - requires predefined jsons to be instantiated
+getPSSASubset <- function(cdm, index, marker, subset_name, subset_id, cohort_table = NULL, table_name = "pssa", study_time = NULL, confidence_interval_level = 0.025){
+  cdm[["subset"]] <- cdm[[subset_name]] %>% filter(cohort_definition_id == subset_id)
+  subset_cdm <- cdmSubsetCohort(cdm, "subset")
+  subset_result <- getPSSA(cdm = subset_cdm,
+                           index = index, 
+                           marker = marker,
+                           cohort_table = cohort_table, 
+                           table_name = table_name, 
+                           study_time = study_time, 
+                           confidence_interval_level = confidence_interval_level) 
+  return(subset_result)
+}
+
 # ### Intake two IDs and generate two cohort sets using capr
 # generatePSSACohortDefinitions <- function (DrugId){
 #   cohort(
