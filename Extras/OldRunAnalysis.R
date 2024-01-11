5b4cf7889c6731b441a5c455372fe5dd3a99333c
xihang-chen <xihang.chen@ndorms.ox.ac.uk>
2024-01-11 15:44:42 +0000

    update

---
+++ Extras/OldRunAnalysis.R
@@ -0,0 +1,48 @@
+# create logger
+log_file <- paste0(output_folder, "/log.txt")
+logger <- create.logger()
+logfile(logger) <- log_file
+level(logger) <- "INFO"
+
+### PSSA
+results_pssa <- getPSSA(cdm = cdm,
+                        index = list(c("amiodarone", "ingredient")), 
+                        marker = list(c("levothyroxine", "ingredient")), 
+                        table_name = "pssa_amiodarone_levothyroxine",
+                        study_time = 730) 
+
+### plots
+getHistogram(results_pssa, "days")
+
+### Waiting Time Distribution
+getWaitingTimeDistribution(cdm = cdm,
+                           drug= list(c("amiodarone", "ingredient")),
+                           table_name = "wtd_amiodarone", 
+                           start_date = "2010-01-01", 
+                           end_date = "2022-12-31", 
+                           prior_obs = 365)
+
+### Subset by JSON
+cohortSet <- readCohortSet(
+  path = here("1_InstantiateCohorts", "CohortPSSA")
+)
+cdm <- generateCohortSet(
+  cdm = cdm, cohortSet = cohortSet, name = "pssa_subset",
+  overwrite = TRUE
+)
+
+results_subset <- getPSSASubset(cdm = cdm, 
+                                index = list(c("amiodarone", "ingredient")), 
+                                marker = list(c("levothyroxine", "ingredient")),
+                                subset_name = "pssa_subset",
+                                subset_id = 1,
+                                study_time = 365)
+
+### subset by sex and/or age
+stratified_pssa_results <- getPSSAStrata(cdm = cdm,
+                                         ageGroup = list(c(0,50), c(50,150), c(0,150)),
+                                         sex = c("Male", "Female", "Both"),
+                                         index = list(c("amiodarone", "ingredient")), 
+                                         marker = list(c("levothyroxine", "ingredient")),
+                                         start_date = NA,
+                                         end_date = NA)
