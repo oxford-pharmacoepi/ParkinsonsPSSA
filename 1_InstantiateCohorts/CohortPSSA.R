52b930e44e115f743b8d3fcbe8bce9f5f8cc7f1e
xihang-chen <xihang.chen@ndorms.ox.ac.uk>
2024-04-19 22:09:44 +0100

    update

--- 1_InstantiateCohorts/CohortPSSA.R
+++ 1_InstantiateCohorts/CohortPSSA.R
@@ -1,6 +1,15 @@
+#### pull back instantiated cohorts
+cdm <- CDMConnector::cdm_from_con(
+  con = db,
+  cdm_schema = cdm_database_schema,
+  write_schema = c(schema = results_database_schema, prefix = stem_table), 
+  cohort_tables = c("parkinson_subtypes", "amiodarone", "levothyroxine", "allopurinol")
+)
+
 ###### Parkinsonism subtypes
 print(paste0("Instantiating Parkinsonism subtypes cohorts at ", Sys.time()))
 outcome_cohorts_subtypes <- readCohortSet(here(
+  "ParkinsonsPSSA",
   "1_InstantiateCohorts",
   "CohortPSSA"
 ))
@@ -21,11 +30,10 @@ print(paste0("Generated levodopa cohort at ", Sys.time()))
 
 # Dopamine Agonists
 print(paste0("Generating dopamine agonists at ", Sys.time()))
-cdm <- getSingleDrugCohort(cdm = cdm,
-                           drug = list(c("Dopamine agonists", "ATC 4th")),
-                           table_name = "dopamine_agonists",
-                           start_date = as.Date("2008-01-01"),
-                           end_date = as.Date("2021-12-31"))
+cdm <- DrugUtilisation::generateAtcCohortSet(cdm = cdm, 
+                                             name = "dopamine_agonists", 
+                                             atcName = "dopamine agonists", 
+                                             level = c("ATC 4th"))
 print(paste0("Generated dopamine agonists at ", Sys.time()))
 
 # Amantadine
@@ -36,37 +44,35 @@ cdm <- DrugUtilisation::generateIngredientCohortSet(cdm = cdm,
 print(paste0("Generated amantadine cohort at ", Sys.time()))
 
 #MAOB
-print(paste0("Generating MAOB at ", Sys.time()))
-cdm <- getSingleDrugCohort(cdm = cdm,
-                           drug = list(c("Monoamine oxidase B inhibitors", "ATC 4th")),
-                           table_name = "MAOB_inhibitors",
-                           start_date = as.Date("2008-01-01"),
-                           end_date = as.Date("2021-12-31"))
-print(paste0("Generated MAOB at ", Sys.time()))
+print(paste0("Generating MAOB cohort at ", Sys.time()))
+cdm <- DrugUtilisation::generateAtcCohortSet(cdm = cdm, 
+                                             name = "maob_inhibitors", 
+                                             atcName = "Monoamine oxidase B inhibitors", 
+                                             level = c("ATC 4th"))
+print(paste0("Generated MAOB cohort at ", Sys.time()))
 
 #COMT
-print(paste0("Generating COMT at ", Sys.time()))
-cdm <- getSingleDrugCohort(cdm = cdm,
-                           drug = list(c("levodopa, decarboxylase inhibitor and COMT inhibitor; oral", "ATC 5th")),
-                           table_name = "COMT_inhibitors",
-                           start_date = as.Date("2008-01-01"),
-                           end_date = as.Date("2021-12-31"))
-print(paste0("Generated COMT at ", Sys.time()))
+print(paste0("Generating COMT cohort at ", Sys.time()))
+cdm <- DrugUtilisation::generateAtcCohortSet(cdm = cdm, 
+                                             name = "comt_inhibitors", 
+                                             atcName = "levodopa, decarboxylase inhibitor and COMT inhibitor; oral", 
+                                             level = c("ATC 5th"))
+print(paste0("Generated COMT cohort at ", Sys.time()))
 
 #CCB
-cdm <- getSingleDrugCohort(cdm = cdm,
-                           drug = list(c("CALCIUM CHANNEL BLOCKERS", "ATC 2nd")),
-                           table_name = "ccb_atc2",
-                           start_date = as.Date("2008-01-01"),
-                           end_date = as.Date("2021-12-31"))
-# 
-# #ANTIDEPRESSANTS
-# cdm <- getSingleDrugCohort(cdm = cdm,
-#                            drug = list(c("ANTIDEPRESSANTS", "ATC 3rd")),
-#                            table_name = "antidepressants",
-#                            start_date = as.Date("2008-01-01"),
-#                            end_date = as.Date("2021-12-31"))
-# 
+print(paste0("Generating ccb cohort at ", Sys.time()))
+cdm <- DrugUtilisation::generateAtcCohortSet(cdm = cdm,
+                                             name = "ccb",
+                                             atcName = "calcium channel blockers",
+                                             level = c("ATC 2nd"))
+print(paste0("Generated ccb cohort at ", Sys.time()))
+
+#ANTIDEPRESSANTS
+print(paste0("Generating antidepressants cohort at ", Sys.time()))
+cdm <- DrugUtilisation::generateAtcCohortSet(cdm = cdm,
+                                             name = "antidepressants",
+                                             atcName = "antidepressants",
+                                             level = c("ATC 3rd"))
 # #ANTIEMETICS
 # cdm <- getSingleDrugCohort(cdm = cdm,
 #                            drug = list(c("ANTIEMETICS AND ANTINAUSEANTS", "ATC 2nd")),
@@ -84,11 +90,9 @@ cdm <- getSingleDrugCohort(cdm = cdm,
 ######## potential antiparkinson cascades
 # CCB
 print(paste0("Generating CCB at ", Sys.time()))
-cdm <- getSingleDrugCohort(cdm = cdm,
-                           drug = list(c("cinnarizine", "ingredient"), c("flunarizine", "ingredient")),
-                           table_name = "ccb",
-                           start_date = as.Date("2008-01-01"),
-                           end_date = as.Date("2021-12-31"))
+cdm <- DrugUtilisation::generateIngredientCohortSet(cdm = cdm,
+                                                    name = "ccb",
+                                                    ingredient = c("cinnarizine", "flunarizine"))
 
 # Dopamine depleters 
 print(paste0("Generating Dopamine Depleters at ", Sys.time()))
