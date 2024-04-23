8ae2152d92bfef81d8037d82eaf4de84d9b2dbb8
xihang-chen <xihang.chen@ndorms.ox.ac.uk>
2024-04-23 16:33:34 +0100

    updating cohorts

--- 2_Analysis/HypothesisDrivenDrugInduced.R
+++ 2_Analysis/HypothesisDrivenDrugInduced.R
@@ -43,3 +43,39 @@ cdm <- DrugUtilisation::generateIngredientCohortSet(cdm = cdm,
                                                                    "pimozide", 
                                                                    "sulpiride"))
 
+#####
+print(paste0("Generating drug cohorts that infrequently causes parkinson-like symptoms at ", Sys.time()))
+# Atypical antipsychotics 
+print(paste0("Generating atypical antipsychotics at ", Sys.time()))
+cdm <- DrugUtilisation::generateIngredientCohortSet(cdm = cdm,
+                                                    name = "atypical_antipsychotics",
+                                                    ingredient = c("clozapine", 
+                                                                   "quetiapine"))
+# Mood stabilizer  
+print(paste0("Generating Mood stabilizer at ", Sys.time()))
+cdm <- DrugUtilisation::generateIngredientCohortSet(cdm = cdm,
+                                                    name = "mood_stabilizer",
+                                                    ingredient = c("lithium"))
+
+# Antidepressant
+print(paste0("Generating antidepressant at ", Sys.time()))
+cdm <- DrugUtilisation::generateIngredientCohortSet(cdm = cdm,
+                                                    name = "mood_stabilizer",
+                                                    ingredient = c("citalopram",
+                                                                   "fluoxetine",
+                                                                   "praoxetine",
+                                                                   "sertraline"))
+# Antiepileptic drugs 
+print(paste0("Generating Antiepileptic drugs at ", Sys.time()))
+cdm <- DrugUtilisation::generateIngredientCohortSet(cdm = cdm,
+                                                    name = "antiepileptics",
+                                                    ingredient = c("phenytoin",
+                                                                   "valproate"))
+
+# Antiemetics Infreqeuncy
+print(paste0("Generating Antiemetics that infrequently causes DIP at ", Sys.time()))
+cdm <- DrugUtilisation::generateIngredientCohortSet(cdm = cdm,
+                                                    name = "antiemetics2",
+                                                    ingredient = c("domperidone",
+                                                                   "itopride",
+                                                                   "clebopride"))
