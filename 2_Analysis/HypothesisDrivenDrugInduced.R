e2a6f374e21b0f6e45fa78b720d5b72b9d518b51
xihang-chen <xihang.chen@ndorms.ox.ac.uk>
2024-04-24 12:05:50 +0100

    hypo driven update

--- 2_Analysis/HypothesisDrivenDrugInduced.R
+++ 2_Analysis/HypothesisDrivenDrugInduced.R
@@ -1,5 +1,26 @@
 print(paste0("Generating drug cohorts that frequently causes parkinson-like symptoms at ", Sys.time()))
 
+hypothesise_subfolder <- here(output_folder, "hypothesise")
+if (!dir.exists(hypothesise_subfolder)) {
+  dir.create(hypothesise_subfolder)
+}
+
+hypothesise_results_subfolder <- here(hypothesise_subfolder, "results")
+if (!dir.exists(hypothesise_results_subfolder)) {
+  dir.create(hypothesise_results_subfolder)
+}
+
+hypothesise_gt_subfolder <- here(hypothesise_subfolder, "gt")
+if (!dir.exists(hypothesise_gt_subfolder)) {
+  dir.create(hypothesise_gt_subfolder)
+}
+
+hypothesise_plots_subfolder <- here(hypothesise_subfolder, "plots")
+if (!dir.exists(hypothesise_plots_subfolder)) {
+  dir.create(hypothesise_plots_subfolder)
+}
+
+print(paste0("Starting PSSA for antiparkinsonian drugs at ", Sys.time()))
 # CCB
 print(paste0("Generating CCB at ", Sys.time()))
 cdm <- DrugUtilisation::generateIngredientCohortSet(cdm = cdm,
@@ -48,7 +69,7 @@ print(paste0("Generating drug cohorts that infrequently causes parkinson-like sy
 # Atypical antipsychotics 
 print(paste0("Generating atypical antipsychotics at ", Sys.time()))
 cdm <- DrugUtilisation::generateIngredientCohortSet(cdm = cdm,
-                                                    name = "atypical_antipsychotics",
+                                                    name = "atypical_antipsychotics2",
                                                     ingredient = c("clozapine", 
                                                                    "quetiapine"))
 # Mood stabilizer  
@@ -79,3 +100,13 @@ cdm <- DrugUtilisation::generateIngredientCohortSet(cdm = cdm,
                                                     ingredient = c("domperidone",
                                                                    "itopride",
                                                                    "clebopride"))
+
+cdm <- omopgenerics::bind(
+  cdm$typical_antipsychotics, cdm$atypical_antipsychotics, cdm$dopamine_depleters, cdm$antiemetics, cdm$ccb,
+  name = "freq_hypothesise_driven"
+)
+
+cdm <- omopgenerics::bind(
+  cdm$atypical_antipsychotics2, cdm$mood_stabilizer, cdm$antidepressants, cdm$antiepileptics, cdm$antiemetics2,
+  name = "infreq_hypothesise_driven"
+)
